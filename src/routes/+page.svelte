d16b3dbee18139c6f2c21176d133a6a5256f0d08
justincj <justincgeorge95@gmail.com>
2024-03-22 00:34:00 +0530

    rules

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,33 +1,35 @@
 <div class="content-wrapper">
-    <h2>About</h2>
-    <p>My name in justin george. I am an independent researcher and engineer. I focus on building full stack solution to challenging
-        problems.
-    </p>
+	<h2>About</h2>
+	<p>
+		My name in justin george. I am an independent researcher and engineer. I focus on building full
+		stack solution to challenging problems.
+	</p>
 </div>
+
 <style>
-.content-wrapper {
-   padding:2em; 
-    max-width: 80%;
-    margin:0 auto;
-    /* text-align: center; */
-}
+	.content-wrapper {
+		padding: 2em;
+		max-width: 80%;
+		margin: 0 auto;
+		/* text-align: center; */
+	}
 
-h2 {
-    text-align: center;
-    text-transform: uppercase;
-    margin-bottom: 0.8em;
-}
+	h2 {
+		text-align: center;
+		text-transform: uppercase;
+		margin-bottom: 0.8em;
+	}
 
-p {
-    font-size: 1.2em;
-    font-family:Arial, Helvetica, sans-serif
-}
-@media (max-width :600px) {
-    .content-wrapper {
-        /* background-color: grey; */
-        max-width: 100%;
-        margin:0 0.5rem;
-        padding:0;
-    }
-}
+	p {
+		font-size: 1.2em;
+		font-family: Arial, Helvetica, sans-serif;
+	}
+	@media (max-width: 600px) {
+		.content-wrapper {
+			/* background-color: grey; */
+			max-width: 100%;
+			margin: 0 0.5rem;
+			padding: 0;
+		}
+	}
 </style>
---
+++ src/routes/rules/+page.svelte
@@ -0,0 +1,4 @@
+<h1>Rules to Live By</h1>
+
+<h3>Being a man of depth</h3>
+<h3>Maintaining Honour</h3>
